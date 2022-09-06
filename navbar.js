d3af91b08c175aab84ada14f39be25ad5769aecf
Rebecca Buehler <you@example.com>
2022-09-06 14:45:40 -0700

    adjusting the navbar styling

--- navbar.js
+++ navbar.js
@@ -3,7 +3,7 @@ function Navbar() {
     <>
       <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" aria-current="page" href="#">
             Bad Bank
           </a>
           <button
@@ -19,11 +19,6 @@ function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
               <li className="nav-item">
                 <a className="nav-link" href="#/CreateAccount/">
                   CreateAccount
