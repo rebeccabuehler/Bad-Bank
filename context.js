7d381f8aa5acdc8cd88bda4be8b90c897c6c5bf5
Rebecca Buehler <you@example.com>
2022-09-06 16:07:28 -0700

    added a card that can change based on props and styling

--- context.js
+++ context.js
@@ -2,3 +2,32 @@ const Route = ReactRouterDOM.Route;
 const Link = ReactRouterDOM.Link;
 const HashRouter = ReactRouterDOM.HashRouter;
 const UserContext = React.createContext(null);
+
+function Card(props) {
+    function classes() {
+        const bg = props.bgcolor ? ' bg-' + props.bgcolor : ' ';
+        const txt = props.txtcolor ? ' text-' + props.txtcolor : ' text-white';
+        return 'card mb-3 ' + bg + txt;
+    }
+    return (
+        <div className={classes()} style={{maxWidth: "18rem"}}>
+            <div className="card-header">{props.header}</div>
+            <div className="card-body">
+                {props.title && (<h5 className="card-title">{props.title}</h5>)}
+                {props.text && (<p className="card-text">{props.text}</p>)}
+                {props.body}
+                {props.status && (<div id="createStatus">{props.status}</div>)}
+            </div>
+        </div>
+    );
+}
+
+
+// <div className="card">
+// <div className="card-header">
+// featured </div>
+// <div className="card-body">
+// <h5 className="card-title"> Special Title Treatment</h5>
+// <p className="card-title"> With supporting text below as a natural lead into additional content.</p>
+// </div>
+// </div>
--- home.js
+++ home.js
@@ -1,9 +1,12 @@
 function Home() {
-    const ctx = React.useContext(UserContext);
     return(
-        <h1>
-            Home <br/>
-            {JSON.stringify(ctx)}
-        </h1>
+        <Card
+        bgcolor="primary"
+        txtcolor="white"
+        header="Bad Bank Landing Page"
+        title="Welcome to the Bank"
+        text="You can use this bank"
+        body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+        />
     );
 }
