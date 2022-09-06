b0528c38ea6bd3232a62b26b506f4e225c28d7ea
Rebecca Buehler <you@example.com>
2022-09-06 14:40:07 -0700

    adding better styling to the navbar

--- navbar.js
+++ navbar.js
@@ -1,43 +1,63 @@
 function Navbar() {
   return (
     <>
-      <ul className="nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">
-            Home
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Bad Bank
           </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#/CreateAccount/">
-            CreateAccount
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#/login/">
-            Login
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#/deposit/">
-            Deposit
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#/withdraw/">
-            Withdraw
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#/balance/">
-            Balance
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#/alldata/">
-            AllData
-          </a>
-        </li>
-      </ul>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#/CreateAccount/">
+                  CreateAccount
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#/login/">
+                  Login
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#/deposit/">
+                  Deposit
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#/withdraw/">
+                  Withdraw
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#/balance/">
+                  Balance
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#/alldata/">
+                  AllData
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
