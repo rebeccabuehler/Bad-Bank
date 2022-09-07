d0ea562e5c843630b5ff6b809f59e4c45e681c6a
Rebecca Buehler <you@example.com>
2022-09-06 17:29:36 -0700

    adding card to create account and login, adding validation

--- createaccount.js
+++ createaccount.js
@@ -1,9 +1,64 @@
 function CreateAccount() {
-    const ctx = React.useContext(UserContext);
-    return(
-            <h1>
-                Create Account <br/>
-                {JSON.stringify(ctx)}
-            </h1>
-    );
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [link, setLink] = React.useState('');
+  const ctx = React.useContext(UserContext);
+
+  function validate(field, label) {
+    if(!field) {
+        setStatus('Error ' + label);
+        setTimeout(() => setStatus(''), 3000);
+        return false;
+    }
+    return true;
+  }
+
+  function handleCreate() {
+    console.log(name, email, password);
+    if(!validate(name, 'name')) return;
+    if(!validate(email, 'email')) return;
+    if(!validate(password, 'password')) return;
+    ctx.users.push({name, email, password, balance: 100});
+    setShow(false);
+  }
+
+    function clearForm() {
+        setName('');
+        setEmail('');
+        setPassword('');
+        setShow(true);
+    }
+
+    function foo() {
+        setLink('/#/login/');
+    }
+
+  return (
+    <Card
+      bgcolor="primary"
+      header="Create Account"
+      status={status}
+      body={show ? (
+            <>
+            Name <br/>
+            <input type="input" className="form-control" id="name" placeholder="Enter Name" value={name} onChange={e => setName(e.currentTarget.value)}/><br/>
+            Email Address<br/>
+            <input type="input" className="form-control" id="email" placeholder="Enter Email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+            Password<br/>
+            <input type="password" className="form-control" id="password" placeholder="Enter Password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
+            <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+            </>
+        ) : ( 
+            <>
+            <h5>Success</h5>
+            <button type="submit" className="btn btn-light" onClick={clearForm}>Add Another Account</button>
+            <br/>
+            <a href="#/login/" className="btn btn-light">Go to Login</a>
+            </>
+        )}
+    />
+  );
 }
